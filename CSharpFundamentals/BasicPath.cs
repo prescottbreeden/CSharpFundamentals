52fc94d80f26048e9cbbc4b8bf698715da3ea918
prescottbreeden <prescott.breeden@gmail.com>
2018-11-24 16:39:33 -0800

    BasicPaths
    working with basic Path builtins

---
+++ CSharpFundamentals/BasicPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpFundamentals
+{
+    class BasicPath
+    {
+        public static void Run()
+        {
+            string path = @"c:\Users\andro\source\repos\CSharpFundamentals\CSharpFundamentals.sln";
+
+            int dotIndex = path.IndexOf('.');
+            string extension = path.Substring(dotIndex);
+            Console.WriteLine(extension);
+
+            string ext = Path.GetExtension(path);
+            Console.WriteLine("Extension: " + ext);
+
+            string fn = Path.GetFileName(path);
+            Console.WriteLine("File Name: " + fn);
+
+            string fn2 = Path.GetFileNameWithoutExtension(path);
+            Console.WriteLine("File Name w/o Extension: " + fn2);
+
+            string dn = Path.GetDirectoryName(path);
+            Console.WriteLine("Directory Name: " + dn);
+        }
+        
+    }
+}
